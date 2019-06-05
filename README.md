d41d5971703983cfcae1a73c5c2cc9041d6baf89
Andrew Marentis <amarentis@usrs.noreply.github.com>
2019-06-05 17:06:43 -0400

    adding customizations

--- README.md
+++ README.md
@@ -5,7 +5,11 @@
 * Virtualbox
 * Vagrant
 
-# Getting started 
+* molecule (optional but preferred)
+
+
+# Getting started with Vagrant
+
 
 1. Run:
 
@@ -16,10 +20,27 @@ vagrant up
 
 3. Save changes
 
-4. check changes run: 
+4. check changes run:
 
 vagrant provision
 
 5. Repeat
+
+
+# Getting Started with molecule
+
+1. Setup
+   pip install molecule testinfra pytest pluggy paramiko flake8 ansible-lint
+
+2. Run
+ molecule init
+ molecule converge
+
+3. To run just tests
+ molecule verify
+
+4. start up from scratch and install everything then run tests
+  molecule test
+
 
 
--- tests/test_default.py
+++ tests/test_default.py
@@ -1,5 +1,6 @@
 import testinfra.utils.ansible_runner
 
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
@@ -17,7 +18,26 @@ def test_nginx_is_installed(host):
     assert nginx.version.startswith("1.12")
 
 
+def test_nginx_conf_file(host):
+    f = host.file('/etc/nginx/nginx.conf')
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_nginx_index_file(host):
+    f = host.file('/usr/share/nginx/html/index.html')
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
 def test_nginx_running_and_enabled(host):
     nginx = host.service("nginx")
     assert nginx.is_running
     assert nginx.is_enabled
+
+
+def test_nginx_accepting_connections(host):
+    nginx = host.socket("tcp://0.0.0.0:80")
+    assert nginx.is_listening
